6a6cd29b17edb660392514063fcc08e3b01db830
oways-work <owayswork@gmail.com>
2025-11-10 15:05:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,5 @@
 #include "shell.h"
 
-/**
- * main - Entry point for the simple shell.
- * @argc: Argument count (unused for this task).
- * @argv: Argument vector (used for printing the program's name in errors).
- *
- * Return: 0 on success, or 1 on error.
- */
 int main(int argc, char **argv)
 {
 	char *line = NULL;
@@ -14,32 +7,46 @@ int main(int argc, char **argv)
 	ssize_t read_bytes;
 	pid_t child_pid;
 	int status;
+	char *args[64];
+	char *token;
+	int i;
+	const char *delim = " \t\n";
+
 	(void)argc;
-	
+
 	while (1)
 	{
-		
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "($) ", 4);
 
-	
 		read_bytes = getline(&line, &len, stdin);
-
 
 		if (read_bytes == -1)
 		{
 			if (isatty(STDIN_FILENO))
-	
+				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
 
-	
 		line[strcspn(line, "\n")] = '\0';
-
 
 		if (line[0] == '\0')
 			continue;
 
+		i = 0;
+		token = strtok(line, delim);
+		while (token != NULL)
+		{
+			args[i] = token;
+			i++;
+			token = strtok(NULL, delim);
+		}
+		args[i] = NULL;
+
+		if (args[0] == NULL)
+		{
+			continue;
+		}
 
 		child_pid = fork();
 		if (child_pid == -1)
@@ -50,13 +57,8 @@ int main(int argc, char **argv)
 
 		if (child_pid == 0)
 		{
-
-			execve(args[0], args, environ)
-
-
-			if (execve(line, exec_argv, environ) == -1)
+			if (execve(args[0], args, environ) == -1)
 			{
-
 				perror(argv[0]);
 				free(line);
 				exit(1);
@@ -64,11 +66,9 @@ int main(int argc, char **argv)
 		}
 		else
 		{
-		
 			wait(&status);
 		}
 	}
-
 
 	free(line);
 	return (0);
