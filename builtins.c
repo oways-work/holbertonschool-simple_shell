9541cd3ccffd60ef904cba6a40404e6c69e911e7
oways-work <owayswork@gmail.com>
2025-11-11 19:57:50 +0300

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -20,15 +20,16 @@ void print_env(void)
  * handle_builtin - Checks for and handles built-in commands.
  * @args: The array of command and arguments.
  * @line: The original line buffer (to free on exit).
+ * @last_status: The exit status of the previously executed command.
  *
  * Return: 1 if a built-in was handled, 0 otherwise.
  */
-int handle_builtin(char **args, char *line)
+int handle_builtin(char **args, char *line, int last_status)
 {
 	if (strcmp(args[0], "exit") == 0)
 	{
 		free(line);
-		exit(0);
+		exit(last_status); /* Exit with the last command's status */
 	}
 
 	if (strcmp(args[0], "env") == 0)
