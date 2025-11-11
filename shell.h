e15ddff38593e01279e85fea664b00ee3957475c
oways-work <owayswork@gmail.com>
2025-11-11 13:06:53 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,21 +1,24 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
+#ifndef SHELL_H
+#define SHELL_H
 
-/*--- Libraries ---*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
 
-/*--- Global Variables ---*/
-/**
- * extern char **environ - The global environment variables.
- * This is necessary for passing the environment to execve.
- */
 extern char **environ;
 
-#endif /* _SHELL_H_ */
+char *find_command_path(char *command);
+char *_getenv(const char *name);
+char *_strdup(const char *s);
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(charT *dest, const char *src);
+int _strcmp(const char *s1, const char *s2);
+char *_strchr(const char *s, int c);
+
+#endif /* SHELL_H */
