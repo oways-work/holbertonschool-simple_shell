c1464ecd4bdc9a9982127cd039c5a1e74c76d757
oways-work <owayswork@gmail.com>
2025-11-11 18:26:00 +0300

    Create string_helpers.c

---
+++ string_helpers.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _strcpy - Copies a string.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ *
+ * Return: A pointer to the destination buffer.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strdup - Duplicates a string using _strlen and _strcpy.
+ * @str: The string to duplicate.
+ *
+ * Return: A pointer to the newly allocated duplicate string,
+ * or NULL if malloc fails or str is NULL.
+ */
+char *_strdup(const char *str)
+{
+	char *dup;
+	size_t len;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = _strlen(str);
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
+		return (NULL);
+
+	_strcpy(dup, str);
+	return (dup);
+}
