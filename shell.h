d0e2be51d450395328faf900eaeb8fbfd6bb693e
oways-work <owayswork@gmail.com>
2025-11-11 18:21:59 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -3,63 +3,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
+#include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
-#include <signal.h>
+#include <errno.h>
 
 extern char **environ;
-extern int last_exit_status;
 
-/* shell.c */
-void shell_loop(void);
-char *read_line(void);
-char **split_line(char *line);
+/* main.c */
+/* (main function) */
 
-/* execute.c */
-int execute(char **args);
+/* find_path.c */
+char *find_command_path(char *command);
 
-/* utils.c */
-char *_getenv(char *name);
-char *find_path(char *command);
-void free_args(char **args);
+/* env_helpers.c */
+char *_getenv(const char *name);
 
-/* string_utils.c */
-int _atoi(char *str);
-int is_number(char *str);
+/* string_helpers.c */
+char *_strdup(const char *str);
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
 
-/* builtins.c */
-int builtin_env(void);
-int builtin_exit(char **args);
-int check_builtin(char **args);
+/* string_helpers2.c */
+char *_strcat(char *dest, const char *src);
+int _strcmp(const char *s1, const char *s2);
+char *_strchr(const char *s, int c);
+int _strncmp(const char *s1, const char *s2, size_t n);
 
-/* env_builtins.c */
-int _setenv(char *name, char *value, int overwrite);
-int _unsetenv(char *name);
-int builtin_setenv(char **args);
-int builtin_unsetenv(char **args);
-
-/* cd_builtin.c */
-int builtin_cd(char **args);
-
-/* help.c */
-int builtin_help(char **args);
-
-/* signal_handler.c */
-void sigint_handler(int sig);
-void setup_signal_handlers(void);
-
-/* comment_handler.c */
-void remove_comment(char *line);
-
-/* variable_expansion.c */
-char *expand_variables(char *line);
-char *process_line_variables(char *line);
-
-/* error_handler.c */
-void print_error(char *program_name, char *cmd, char *msg);
-int get_exit_status(int status);
 
 #endif /* SHELL_H */
