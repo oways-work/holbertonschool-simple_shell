88d8e9bed1b8d2390a999553deeac58c4aa8a283
oways-work <owayswork@gmail.com>
2025-11-11 19:42:04 +0300

    Update main.c

--- main.c
+++ main.c
@@ -12,81 +12,46 @@ int main(int argc, char **argv)
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read_bytes;
-	pid_t child_pid;
-	int status;
 	char *args[64];
-	char *token;
-	int i;
+	char *token, *command_path;
 	const char *delim = " \t\n";
-	char *command_path;
+	int i, built_in_status;
 
 	(void)argc;
-
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "($) ", 4);
-
 		read_bytes = getline(&line, &len, stdin);
-
 		if (read_bytes == -1)
 		{
 			if (isatty(STDIN_FILENO))
 				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
-
-		line[strcspn(line, "\n")] = '\0';
-
-		if (line[0] == '\0')
+		if (line[0] == '\n')
 			continue;
-
 		i = 0;
 		token = strtok(line, delim);
 		while (token != NULL)
 		{
-			args[i] = token;
-			i++;
+			args[i++] = token;
 			token = strtok(NULL, delim);
 		}
 		args[i] = NULL;
-
 		if (args[0] == NULL)
 			continue;
-
+		built_in_status = handle_builtin(args, line);
+		if (built_in_status == 1)
+			continue;
 		command_path = find_command_path(args[0]);
-
 		if (command_path == NULL)
 		{
 			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args[0]);
 			continue;
 		}
-
-		child_pid = fork();
-		if (child_pid == -1)
-		{
-			perror("fork");
-			continue;
-		}
-
-		if (child_pid == 0)
-		{
-			if (execve(command_path, args, environ) == -1)
-			{
-				perror(argv[0]);
-				free(command_path);
-				free(line);
-				exit(1);
-			}
-		}
-		else
-		{
-			wait(&status);
-			if (_strcmp(command_path, args[0]) != 0)
-				free(command_path);
-		}
+		execute_command(args, argv, command_path, line);
 	}
-
 	free(line);
 	return (0);
 }
