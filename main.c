6e319f65d6d8950180e999b1146031bdb42a48cb
oways-work <owayswork@gmail.com>
2025-11-11 17:41:57 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,87 +1,58 @@
 #include "shell.h"
 
+int last_exit_status = 0;
+
+/**
+ * main - Entry point for simple shell
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: exit status
+ */
 int main(int argc, char **argv)
 {
 	char *line = NULL;
+	char **args;
 	size_t len = 0;
-	ssize_t read_bytes;
-	pid_t child_pid;
-	int status;
-	char *args[64];
-	char *token;
-	int i;
-	const char *delim = " \t\n";
-	char *command_path;
+	int builtin_status;
 
 	(void)argc;
+	(void)argv;
+	setup_signal_handlers();
 
-	while (1)
+	if (isatty(STDIN_FILENO))
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "($) ", 4);
-
-		read_bytes = getline(&line, &len, stdin);
-
-		if (read_bytes == -1)
+		shell_loop();
+	}
+	else
+	{
+		while (getline(&line, &len, stdin) != -1)
 		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			break;
-		}
-
-		line[strcspn(line, "\n")] = '\0';
-
-		if (line[0] == '\0')
-			continue;
-
-		i = 0;
-		token = strtok(line, delim);
-		while (token != NULL)
-		{
-			args[i] = token;
-			i++;
-			token = strtok(NULL, delim);
-		}
-		args[i] = NULL;
-
-		if (args[0] == NULL)
-		{
-			continue;
-		}
-
-		command_path = find_command_path(args[0]);
-
-		if (command_path == NULL)
-		{
-			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args[0]);
-			continue;
-		}
-
-		child_pid = fork();
-		if (child_pid == -1)
-		{
-			perror("fork");
-			continue;
-		}
-
-		if (child_pid == 0)
-		{
-			if (execve(command_path, args, environ) == -1)
+			remove_comment(line);
+			args = split_line(line);
+			if (args[0] != NULL)
 			{
-				perror(argv[0]);
-				free(command_path);
-				free(line);
-				exit(1);
+				builtin_status = check_builtin(args);
+				if (builtin_status <= -2)
+				{
+					last_exit_status = -2 - builtin_status;
+					free_args(args);
+					break;
+				}
+				else if (builtin_status == -1)
+				{
+					free_args(args);
+					break;
+				}
+				else if (builtin_status == 0)
+				{
+					last_exit_status = execute(args);
+				}
 			}
+			free_args(args);
 		}
-		else
-		{
-			wait(&status);
-			if (command_path != args[0])
-				free(command_path);
-		}
+		free(line);
 	}
 
-	free(line);
-	return (0);
+	return (last_exit_status);
 }
